7640d8ee0d57b688dc69fc1d7b982c429722093e
Otabek1905 <otaw1234@gmail.com>
2022-06-07 12:45:24 +0500

    to merge

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,10 +1,16 @@
 package com.company;
 
+import java.util.Random;
+
 class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
         System.out.println("Nima gap");
+
+
+        Random random = new Random();
+        System.out.println(random.nextInt(100, 1000));
 
     }
 }
